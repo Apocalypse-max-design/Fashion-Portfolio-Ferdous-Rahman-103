effe1048004c9a93a970c3edfe0ea5222b167605
Apocalypse-max-design <mdferdousrahman375@gmail.com>
2026-07-26 19:39:49 +0600

    Fix broken imports in ContactSection

---
+++ components/IconExports.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { Instagram, Facebook, Github } from "lucide-react";
+
+export { LinkedinIcon, InstagramIcon as Instagram, FacebookIcon as Facebook, GithubIcon as Github } from "./Icons";
+
+// Re-export for ContactSection
+export const InstagramIcon = Instagram;
+export const FacebookIcon = Facebook;
+export const GithubIcon = Github;
